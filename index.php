f15087fd990e139ab4c38ff4daf301566eb31255
ArunSinghJ <accord.j7@gmail.com>
2017-03-09 19:07:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div align="center">
                 <h1>Hello Arun</h1>
                 <h2>Congratulations! You have successfully completed the Certification!</h2>
-                <div align="center"><img src="https://s3.amazonaws.com/myawscertificate/Arunsingh_Jeyasingh_Jacob_AWS_SolutionArchitect_Associate.pdf"></div>
+                <div align="center"><img src="https://s3.us-east-2.amazonaws.com/arunsnewbucket/black-ferrari-wallpaper-images.jpg"></div>
                 <p>You Arun's Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
