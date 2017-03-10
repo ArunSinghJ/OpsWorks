48a8b9b221931d727e2f6f0bc790bdd0ed9ebe3b
ArunSinghJ <accord.j7@gmail.com>
2017-03-09 19:04:52 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Arun</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello Arun</h1>
+                <h2>Congratulations! You have successfully completed the Certification!</h2>
+                <div align="center"><img src="https://s3.amazonaws.com/myawscertificate/Arunsingh_Jeyasingh_Jacob_AWS_SolutionArchitect_Associate.pdf"></div>
+                <p>You Arun's Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
